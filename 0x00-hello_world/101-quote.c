aeb7bd294c2bf2018379261562cced041a35d5c3
DidierShemaGatete <didiershemagate01@gmail.com>
2023-02-20 14:15:32 -0800

    collections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - A c program that prints a line to the standard error
  *
- * return: Always 1 (Success)
+ * return 1 (Success)
  */
 int main(void)
 {
