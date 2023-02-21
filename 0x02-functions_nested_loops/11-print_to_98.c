1b3bf0a929db7d8741598a652bed404ed5eb3c57
DidierShemaGatete <didiershemagate01@gmail.com>
2023-02-21 09:14:07 -0800

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,7 @@
  * Return: Always 0.
  */
 void print_to_98(int n)
+
 {
 	if (n <= 98)
 	{
@@ -31,7 +32,7 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	printf("%d, ", n);
+	printf("%d,", n);
 	printf("\n");
 	break;
 	}
