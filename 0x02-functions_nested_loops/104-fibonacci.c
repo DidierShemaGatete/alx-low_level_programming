b4f1664ff2ee394a1f1513f730a8d985bc2a2f5c
DidierShemaGatete <didiershemagate01@gmail.com>
2023-02-22 11:52:18 -0800

    a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with
- * 1 and 2, separated by a comma followed by a space.
- * Return: Always 0.
+ * main - fibonacci <3
+ * Purpose - no hardcore
+ *
+ * Return (Success)
  */
 
 int main(void)
-
 {
 	int count;
+
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
