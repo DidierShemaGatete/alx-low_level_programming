8db14f9476b16795bc4d49e007ccbdcde90a2551
DidierShemaGatete <didiershemagate01@gmail.com>
2023-02-23 06:52:17 -0800

    C functions and nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print the numbers since 0 up to 9
+ * Decription: Prints the numbers excluding 2 and 4
+ * Return: The numbers since 0 up to 9
+ */
+
+void print_most_numbers(void)
+
+{
+	int x = o;
+
+	for (; x <= 9; x++)
+	{
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ functions, more nested loops
