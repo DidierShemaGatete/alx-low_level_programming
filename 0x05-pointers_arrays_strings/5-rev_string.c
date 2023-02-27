c9b85ab397e04f25717da73e3fd05c3d5e82720e
DidierShemaGatete <didiershemagate01@gmail.com>
2023-02-27 07:49:57 -0800

    a function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_putchar -> reversing  a string
+ * @s: parameter s
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length1, lenght2;
+
+	lenght1 = 0;
+	lenght2 = 0;
+
+	while (s[lenght1] != '\n')
+		lenght1++;
+	lenght2 = lenght - 1
+	for (i = 0; i < lenght1 / 2; i++)
+	{
+		"samalx" "xlamas"
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
+	}
+}
