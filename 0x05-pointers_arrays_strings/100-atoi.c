143f95fbaa384e1613992c701f574deaebb710bc
DidierShemaGatete <didiershemagate01@gmail.com>
2023-02-27 16:03:29 -0800

    a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,37 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
+ * _atoi - converts a string to an integer
+ * @s: the pointer to convert
  *
- * Return: A pointer to the encoded string.
+ * Return: an integer
  */
-char *rot13(char *str)
+int _atoi(char *s)
+
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	char rot13key[52] = {"N","O","P","Q","R","S","T","U","V","W","C","Y","Z","A","B","C","D","E","F","G","H","I","J","K","L","M","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m"};
-
-	while (str[indx1])
+	while (s[c])
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
-		{
-			if (str [indx1] == alphabet[indx2])
-			{
-				str[indx1] = rot13key[indx2];
-				break;
-			}
-		}
-
-		indx1++;
+	if (s[c] == 45)
+	{
+	min *= -1;
 	}
-
-	return (str);
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	isi = 1;
+	ni = (ni * 10) + (s[c] - '0');
+	c++
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= min;
+	return (ni);
 }
