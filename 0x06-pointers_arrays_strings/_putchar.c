98773e1090e5837ab0df3a719b5a555f2736b1a2
DidierShemaGatete <didiershemagate01@gmail.com>
2023-03-01 13:26:49 -0800

    corrections

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,9 +1,9 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ *_putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
