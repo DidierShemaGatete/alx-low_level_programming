e382c51c5c1e99f3ed3b0140ecee716c853da2e4
DidierShemaGatete <didiershemagate01@gmail.com>
2023-03-06 13:50:07 -0800

    a function that copies memory area.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,21 +10,21 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+    unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-		        printf("\n");
-		}
-			printf("0x%02x", buffer[i]);
-			i++;
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
         }
-        printf("\n");
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
 }
