0c11835804dae078db8469b10c3caaca8aa33508
DidierShemaGatete <didiershemagate01@gmail.com>
2023-03-08 11:46:49 -0800

    debugging

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * return: Always 0 (Success)
- */
-int main (void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
