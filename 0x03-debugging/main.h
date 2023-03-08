6f3fb31714a6c02d510a52b1489676d2ce95ee00
DidierShemaGatete <didiershemagate01@gmail.com>
2023-03-08 10:59:58 -0800

    debugging 1-main.c file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -11,4 +10,4 @@ int convert_day(int month, int day);
 
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+#endif
