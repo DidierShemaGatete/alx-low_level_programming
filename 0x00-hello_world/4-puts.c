508e24c1793ca5fe831481c73e83163f29a89ee4
DidierShemaGatete <didiershemagate01@gmail.com>
2023-03-08 11:53:12 -0800

    debugging

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
