59395b6b8c04dd837ca52c8f51e24ea387fe3a52
DidierShemaGatete <didiershemagate01@gmail.com>
2023-03-20 07:36:53 -0700

    a program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
