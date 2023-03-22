45b35f67937447a88f25c2bc4bc0d8854456757c
DidierShemaGatete <didiershemagate01@gmail.com>
2023-03-22 04:53:59 -0700

    Collections on function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,12 +11,12 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
 	};
 
 
@@ -24,7 +24,7 @@ int (*get_op_func(char *s))(int, int)
 
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
-			i++;
+		i++;
 
 
 	return (ops[i].f);
