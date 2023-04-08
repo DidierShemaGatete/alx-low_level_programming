27b36984b0d94bda5de2040983c51a018896094b
DidierShemaGatete <didiershemagate01@gmail.com>
2023-04-08 07:23:02 -0700

    corrections

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if(index >= sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	*n &= ~(1UL << index);
